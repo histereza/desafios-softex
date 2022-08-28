0735e8472b61ca2913e1c19c6f73ca4a32c46b12
Tereza Teles <86265429+histereza@users.noreply.github.com>
2022-08-28 13:47:36 -0300

    calculadora-imc

---
+++ 8diasdedesafios-js/desafio4/main.js
@@ -0,0 +1,26 @@
+function imc() {
+  const nome = document.querySelector('txtboxnome')
+  const peso = parseFloat(document.getElementById('txtboxpeso').value)
+  const altura = parseFloat(document.getElementById('txtboxaltura').value)
+
+  let imc = (peso / (altura * altura)).toFixed(2)
+  let situacao
+
+  if (imc >= 40) {
+    situacao = 'Obesidade grave'
+  } else if (imc >= 30) {
+    situacao = 'Obesidade'
+  } else if (imc >= 25) {
+    situacao = 'Sobrepeso'
+  } else if (imc >= 18.5) {
+    situacao = 'Normal'
+  } else if (imc < 18.5) {
+    situacao = 'Magreza'
+  } else {
+    situacao = 'Por favor, tente digitar valores válidos.'
+  }
+
+  document
+    .getElementById('resultado')
+    .setAttribute('value', `${imc} - ${situacao}`)
+}
